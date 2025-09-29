0f775e3276fb63361f2f047f5e01f30a3908285a
xexgm <1654648222@qq.com>
2025-09-29 17:58:35 +0800

    初始化

---
+++ IM-Bootstrap/src/main/java/com/gm/imbootstrap/ImBootstrapApplication.java
@@ -0,0 +1,13 @@
+package com.gm.imbootstrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImBootstrapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImBootstrapApplication.class, args);
+    }
+
+}
---
+++ IM-Bootstrap/src/test/java/com/gm/imbootstrap/ImBootstrapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gm.imbootstrap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImBootstrapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
